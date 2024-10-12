c98188329a9e62bb25511a704f25d68e3d3019ab
Sebastian Bergman <basti.bergman@proton.me>
2024-10-12 15:17:27 +0300

    fix: build wasn't working. Now it should

--- src/features/ui/AppCustomButton.tsx
+++ src/features/ui/AppCustomButton.tsx
@@ -13,7 +13,6 @@ interface Props {
   startIcon?: React.ReactNode;
   endIcon?: React.ReactNode;
   sx?: SxProps<Theme>;
-  variantTypo?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "subtitle1" | "subtitle2" | "body1" | "body2" | "button";
 }
 
 export default function AppButton({
@@ -28,7 +27,6 @@ export default function AppButton({
   endIcon,
   sx,
   onClick,
-  variantTypo,
 }: Props) {
   return (
     <LoadingButton
